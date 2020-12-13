3a49f6cdf2d6e8ae6c58db325b69613f775e1aa8
Mike Manchee <mikemanchee@gmail.com>
2020-12-13 09:25:18 -0800

    add render to headlines

--- src/components/Headlines.js
+++ src/components/Headlines.js
@@ -31,7 +31,26 @@ class Headlines extends React.Component {
   }
 
   render() {
-    ...
+    const { error, isLoaded, headlines } = this.state;
+    if (error) {
+      return <React.Fragment>Error: {error.message}</React.Fragment>;
+    } else if (!isLoaded) {
+      return <React.Fragment>Loading...</React.Fragment>;
+    } else {
+      return (
+        <React.Fragment>
+          <h1>Headlines</h1>
+          <ul>
+            {headlines.map((headline, index) =>
+              <li key={index}>
+                <h3>{headline.title}</h3>
+                <p>{headline.abstract}</p>
+              </li>
+            )}
+          </ul>
+        </React.Fragment>
+      );
+    }
   }
 }
 
