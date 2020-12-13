6e42492029fddd33fe31a72688b9fff692b2ff09
Mike Manchee <mikemanchee@gmail.com>
2020-12-13 09:23:22 -0800

    add app and headlines with make api call and mount

---
+++ src/components/Headlines.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+class Headlines extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      headlines: []
+    };
+    componentDidMount() {
+      this.makeApiCall()
+    }
+    makeApiCall = () => {
+      fetch(`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${process.env.REACT_APP_API_KEY}`)
+        .then(response => response.json())
+        .then(
+          (jsonifiedResponse) => {
+            this.setState({
+              isLoaded: true,
+              headlines: jsonifiedResponse.results
+            });
+          })
+          .catch((error) => {
+            this.setState({
+              isLoaded: true,
+              error
+            });
+          });
+    }
+  }
+
+  render() {
+    ...
+  }
+}
+
+export default Headlines;
